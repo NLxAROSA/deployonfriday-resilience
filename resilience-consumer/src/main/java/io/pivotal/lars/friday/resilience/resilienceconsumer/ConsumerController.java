d4d2e3328560d51f98eacaa930c9239925504f3e
Lars Rosenquist <lars.rosenquist@kpn.com>
2019-01-13 23:00:03 +0100

    Added resilience4j Bulkhead

--- resilience-consumer/src/main/java/io/pivotal/lars/friday/resilience/resilienceconsumer/ConsumerController.java
+++ resilience-consumer/src/main/java/io/pivotal/lars/friday/resilience/resilienceconsumer/ConsumerController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.Bulkhead;
+import io.github.resilience4j.reactor.bulkhead.operator.BulkheadOperator;
+import reactor.core.publisher.Mono;
+
 /**
  * ConsumerController
  */
@@ -20,8 +24,16 @@ public class ConsumerController {
     }
 
     @GetMapping()
-    public String doSomething() throws RestClientException, URISyntaxException {
+    public String okay()    {
         return "The message was " + restTemplate.getForObject("https://resilience-provider/", String.class);
+    }
+
+    @GetMapping("/slow")
+    public Mono<String> slow()    {
+        Bulkhead bulkhead = Bulkhead.ofDefaults("backendName");
+        return Mono.fromCallable(()-> {
+            return "The message was " + restTemplate.getForObject("https://resilience-provider/slow", String.class);
+        }).transform(BulkheadOperator.of(bulkhead));
     }
     
 }
