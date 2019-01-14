cf95b034fe2b485e35e901db43a418692a7f563f
Lars Rosenquist <lars.rosenquist@kpn.com>
2019-01-14 22:45:37 +0100

    Removed webflux. Fixed a few bugs.

--- resilience-consumer/src/main/java/io/pivotal/lars/friday/resilience/resilienceconsumer/ConsumerController.java
+++ resilience-consumer/src/main/java/io/pivotal/lars/friday/resilience/resilienceconsumer/ConsumerController.java
@@ -1,40 +1,50 @@
 package io.pivotal.lars.friday.resilience.resilienceconsumer;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import io.github.resilience4j.bulkhead.Bulkhead;
-import io.github.resilience4j.reactor.bulkhead.operator.BulkheadOperator;
-import reactor.core.publisher.Mono;
+import io.github.resilience4j.bulkhead.BulkheadConfig;
+import io.vavr.CheckedFunction0;
+import io.vavr.control.Try;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * ConsumerController
  */
 @RestController
+@Slf4j
 public class ConsumerController {
 
     private final RestTemplate restTemplate;
-
     private final String providerUri;
+    private final BulkheadConfig config;
+    private final Bulkhead bulkhead;
 
-    public ConsumerController(RestTemplate restTemplate, @Value("${provider.uri}") String providerUri)    {
+    public ConsumerController(RestTemplate restTemplate, @Value("${provider.uri}") String providerUri,
+            @Value("${maxConcurrent}") int maxConcurrent) {
         this.restTemplate = restTemplate;
         this.providerUri = providerUri;
+        config = BulkheadConfig.custom().maxConcurrentCalls(maxConcurrent).build();
+        bulkhead = Bulkhead.of("resilience-provider", config);
+        bulkhead.getEventPublisher().onCallPermitted(event -> log.info("Call permitted"))
+                .onCallRejected(event -> log.info("Call rejected"));
     }
 
     @GetMapping()
-    public String okay()    {
+    public String okay() {
         return "The message was " + restTemplate.getForObject(providerUri, String.class);
     }
 
     @GetMapping("/slow")
-    public Mono<String> slow()    {
-        Bulkhead bulkhead = Bulkhead.ofDefaults("backendName");
-        return Mono.fromCallable(()-> {
-            return "The message was " + restTemplate.getForObject(providerUri + "/slow", String.class);
-        }).transform(BulkheadOperator.of(bulkhead));
+    public String slow() {
+        CheckedFunction0<String> bulkheadedBackend = Bulkhead.decorateCheckedSupplier(bulkhead,
+                () -> "The message was " + restTemplate.getForObject(providerUri + "/slow", String.class));
+        Try<String> result = Try.of(bulkheadedBackend).recover((throwable) -> "This is a fallback");
+        return result.get();
     }
-    
 }
--- resilience-provider/src/main/java/io/pivotal/lars/friday/resilience/resilience/MessageController.java
+++ resilience-provider/src/main/java/io/pivotal/lars/friday/resilience/resilience/MessageController.java
@@ -3,34 +3,35 @@ package io.pivotal.lars.friday.resilience.resilience;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import reactor.core.publisher.Mono;
-
 /**
  * MessageController
  */
 @RestController
 public class MessageController {
 
+    private static final int SECOND = 1000;
+    private static final int MINUTE = 60000;
+
     @GetMapping("/")
-    public Mono<String> okay()   {
-        return Mono.just("I'm fine.");
+    public String okay() {
+        return "I'm fine.";
     }
 
     @GetMapping("/slow")
-    public Mono<String> slow() throws InterruptedException {
-        Thread.sleep(250);
-        return Mono.just("I'm fine, just slow");
+    public String slow() throws InterruptedException {
+        Thread.sleep(SECOND);
+        return "I'm fine, just slow";
     }
 
     @GetMapping("/extremelyslow")
-    public Mono<String> extremelySlow() throws InterruptedException {
-        Thread.sleep(180000);
-        return Mono.just("I'm not fine, but will respond, most likely after your timeout has expired");
+    public String extremelySlow() throws InterruptedException {
+        Thread.sleep(10 * MINUTE);
+        return "I'm not fine, but will respond, most likely after your timeout has expired";
     }
 
     @GetMapping("/error")
-    public Mono<String> error()  {
+    public String error() {
         throw new InternalServerErrorException("I'm definitely not fine!");
     }
-    
+
 }
